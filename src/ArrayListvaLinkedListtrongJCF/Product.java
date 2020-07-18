444e70ff40b410ae132eef5bccfef3b29566e335
huy8895 <huy8895@gmail.com>
2020-07-19 01:11:00 +0700

    [Bài tập] Luyện tập sử dụng ArrayList, LinkedList trong Java Collection Framework

---
+++ src/ArrayListvaLinkedListtrongJCF/Product.java
@@ -0,0 +1,40 @@
+package ArrayListvaLinkedListtrongJCF;
+
+public class Product {
+    private String productName;
+    private int productID;
+    private int productPrice;
+
+    public Product() {
+    }
+
+    public Product(String productName, int productID, int productPrice) {
+        this.productName = productName;
+        this.productID = productID;
+        this.productPrice = productPrice;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+}
---
+++ src/ArrayListvaLinkedListtrongJCF/ProductManager.java
@@ -0,0 +1,4 @@
+package ArrayListvaLinkedListtrongJCF;
+
+public class ProductManager {
+}

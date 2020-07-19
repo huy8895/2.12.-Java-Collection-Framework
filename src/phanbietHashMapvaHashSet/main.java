0f56ab7e4c68c31e7ee3e08233ea8e1b83bfdddb
huy8895 <huy8895@gmail.com>
2020-07-19 19:18:30 +0700

    [Thực hành] Phân biệt giữa HashMap và HashSet

--- src/phanbietHashMapvaHashSet/main.java
+++ src/phanbietHashMapvaHashSet/main.java
@@ -1,9 +1,6 @@
 package phanbietHashMapvaHashSet;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class main {
     public static void main(String[] args) {
@@ -21,8 +18,12 @@ public class main {
         students.add(new Student("Nam",20,"hanoi"));
         students.add(new Student("hung",21,"hanoi"));
         students.add(new Student("ha",22,"hanoi"));
-        for (Student student: students)
-            System.out.println(student.toString());
+//        for (Student student: students)
+//            System.out.println(student.toString());
+        Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 
 
